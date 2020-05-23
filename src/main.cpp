18e434ed20dbdb329dd3ce050d4a3630bc439c07
Lucas Borowiecki <baconomist@gmail.com>
2020-05-23 13:49:05 -0400

    Fixed Robot API by making it import Robot like Simulator API

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,5 @@
 #include "main.h"
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button()
-{
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed)
-	{
-		pros::lcd::set_text(2, "I was pressed!");
-	}
-	else
-	{
-		pros::lcd::clear_line(2);
-	}
-}
+#include "shared_robot_api.hpp"
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -28,10 +9,7 @@ void on_center_button()
  */
 void initialize()
 {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+    shared_api::initialize();
 }
 
 /**
@@ -39,7 +17,10 @@ void initialize()
  * the VEX Competition Switch, following either autonomous or opcontrol. When
  * the robot is enabled, this task will exit.
  */
-void disabled() {}
+void disabled()
+{
+    shared_api::disabled();
+}
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -50,7 +31,10 @@ void disabled() {}
  * This task will exit when the robot is enabled and autonomous or opcontrol
  * starts.
  */
-void competition_initialize() {}
+void competition_initialize()
+{
+    shared_api::competition_initialize();
+}
 
 /**
  * Runs the user autonomous code. This function will be started in its own task
@@ -63,7 +47,14 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous()
+{
+    shared_api::initialize_autonomous();
+    while(true)
+    {
+        shared_api::update_autonomous();
+    }
+}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -80,20 +71,9 @@ void autonomous() {}
  */
 void opcontrol()
 {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-
-	while (true)
-	{
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-						 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-						 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
-	}
+    shared_api::initialize_opcontrol();
+    while(true)
+    {
+        shared_api::update_opcontrol();
+    }
 }
